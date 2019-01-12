4e267539839343652f2b3c06e0728a6f6a01a722
Vidyabaskar <20946816+vidyabaskar@users.noreply.github.com>
2019-01-12 16:32:49 +0530

    Add files via upload

--- sharktank/app.py
+++ sharktank/app.py
@@ -18,48 +18,132 @@ def search():
         return render_template('search.html')
 
     elif request.method == 'POST':
-        cname = request.form['cname']
-        season = request.form.getlist('season')
-        cat = request.form.getlist('category')
-        deal = request.form.getlist('deal')
-        nosharks = request.form.getlist('nosharks')
-        sharks = request.form.getlist('sharks')
-        gender = request.form.getlist('gender')
-        amount = request.form['amount']
+        cur = mysql.get_db().cursor()
+        cat = request.form.getlist('searchcategory')
+        input = request.form['searchfield']
+        cat2 = request.form.getlist('searchcategory2')
 
-        if season:
-            season = season[0]
-        else:
-            season = ""
         if cat:
             cat = cat[0]
         else:
             cat = ""
-        if deal:
-            deal = deal[0]
-        else:
-            deal = ""
-        if nosharks:
-            nosharks = nosharks[0]
-        else:
-            nosharks = ""
-        if gender:
-            gender = gender[0]
-        else:
-            gender = ""
-        if sharks:
-            sharks = sharks[0]
-        else:
-            sharks = ""
 
-        data = {
-            'cname': cname, 'season': season, 'category': cat, 'deal': deal, 'numsharks': nosharks, 'sharks': sharks,
-            'amount': amount
-        }
-        print(data)
-        cur = mysql.get_db().cursor()
+        if cat2:
+            cat2 = cat2[0]
+        else:
+            cat2 = ""
 
-        
+        if cat:
+            if cat == 'season':
+                if input == '1':
+                    cat = 'season01'
+                elif input == '2':
+                    cat = 'season02'
+                elif input == '3':
+                    cat = 'season03'
+                elif input == '4':
+                    cat = 'season04'
+                elif input == '5':
+                    cat = 'season05'
+                elif input == '6':
+                    cat = 'season06'
+                else:
+                    abort(404)
+                cur.execute('''SELECT * FROM ''' + cat + ' natural join companydetails')
+                rs = cur.fetchall()
+
+            elif cat == 'cname':
+                cur.execute('''SELECT * FROM season01 s natural join companydetails where s.company = \'''' + input + '\'')
+                rs = cur.fetchall()
+                if rs:
+                    print(rs)
+                else:
+                    cur.execute('''SELECT * FROM season02 s natural join companydetails where s.company = \'''' + input + '\'')
+                    rs = cur.fetchall()
+                    print(rs)
+                if rs:
+                    print(rs)
+                else:
+                    cur.execute('''SELECT * FROM season03 s natural join companydetails where s.company = \'''' + input + '\'')
+                    rs = cur.fetchall()
+                if rs:
+                    print(rs)
+                else:
+                    cur.execute('''SELECT * FROM season04 s natural join companydetails where s.company = \'''' + input + '\'')
+                    rs = cur.fetchall()
+                if rs:
+                    print(rs)
+                else:
+                    cur.execute('''SELECT * FROM season05 s natural join companydetails where s.company = \'''' + input + '\'')
+                    rs = cur.fetchall()
+                if rs:
+                    print(rs)
+                else:
+                    cur.execute('''SELECT * FROM season06 s natural join companydetails where s.company = \'''' + input + '\'')
+                    rs = cur.fetchall()
+
+            elif cat == 'deal' or cat == 'category' or cat == 'numsharks':
+                t = list()
+                cur.execute('''SELECT * FROM season01 s natural join companydetails where s.''' + cat +'''   = \'''' + input + '\'')
+                rs = cur.fetchall()
+                cur.execute('''SELECT * FROM season02 s natural join companydetails where s.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season03 s natural join companydetails where s.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season04 s natural join companydetails where s.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season05 s natural join companydetails where s.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season06 s natural join companydetails where s.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                rs = list(rs)
+                for i in t:
+                    for j in i:
+                        rs.append(j)
+                rs = tuple(rs)
+
+            elif cat == 'gender':
+                t = list()
+                cur.execute('''SELECT * FROM season01 s natural join companydetails c where c.''' + cat +'''   = \'''' + input + '\'')
+                rs = cur.fetchall()
+                cur.execute('''SELECT * FROM season02 s natural join companydetails c where c.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season03 s natural join companydetails c where c.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season04 s natural join companydetails c where c.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season05 s natural join companydetails c where c.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                cur.execute('''SELECT * FROM season06 s natural join companydetails c where c.''' + cat +'''   = \'''' + input + '\'')
+                r = cur.fetchall()
+                r = list(r)
+                t.append(r)
+                rs = list(rs)
+                for i in t:
+                    for j in i:
+                        rs.append(j)
+                rs = tuple(rs)
+
+
+        return render_template('results.html',result=rs)
         # Compnay name:
         # Season:
         # Episode premiered:
@@ -70,124 +154,6 @@ def search():
         # Equity:
         # Amount per shark:
         # Total value:
-
-        if cname:
-            cur.execute('''SELECT * FROM season01 natural join companydetails WHERE COMPANY = \''''+ cname + '\'')
-            rs = cur.fetchall()
-            print(rs)
-            if rs == ():
-                cur.execute('''SELECT * FROM season02 WHERE COMPANY = \'''' + cname + '\'')
-                rs = cur.fetchall()
-            if rs == ():
-               cur.execute('''SELECT * FROM season03 WHERE COMPANY = \'''' + cname + '\'')
-                rs = cur.fetchall()
-            if rs == ():
-                cur.execute('''SELECT * FROM season04 WHERE COMPANY = \'''' + cname + '\'')
-                rs = cur.fetchall()
-            if rs == ():
-                cur.execute('''SELECT * FROM season05 WHERE COMPANY = \'''' + cname + '\'')
-                rs = cur.fetchall()
-            if rs == ():
-                cur.execute('''SELECT * FROM season06 WHERE COMPANY = \'''' + cname + '\'')
-                rs = cur.fetchall()
-            for i in rs:
-                if i[3] == 'Yes'
-
-            else:
-                return render_template('results.html',result=rs)
-
-        elif season:
-            if season == '1':
-                cur.execute('''SELECT * FROM season01''')
-                rs = cur.fetchall()
-                rs = list(rs)
-                r = []
-                for i in rs:
-                    i = list(i)
-                    i.insert(1,season)
-                    i = tuple(i)
-                    r.append(i)
-                rs = tuple(r)
-                print(rs)
-                return render_template('results.html', result=rs)
-
-            elif season == '2':
-                cur.execute('''SELECT * FROM season02''')
-                rs = cur.fetchall()
-                rs = list(rs)
-                r = []
-                for i in rs:
-                    i = list(i)
-                    i.insert(1,season)
-                    i = tuple(i)
-                    r.append(i)
-                rs = tuple(r)
-                print(rs)
-                return render_template('results.html', result=rs)
-
-            elif season == '3':
-                cur.execute('''SELECT * FROM season03''')
-                rs = list(rs)
-                r = []
-                for i in rs:
-                    i = list(i)
-                    i.insert(1,season)
-                    i = tuple(i)
-                    r.append(i)
-                rs = tuple(r)
-                print(rs)
-                rs = cur.fetchall()
-                return render_template('results.html', result=rs)
-
-            elif season == '4':
-                cur.execute('''SELECT * FROM season04''')
-                rs = list(rs)
-                r = []
-                for i in rs:
-                    i = list(i)
-                    i.insert(1,season)
-                    i = tuple(i)
-                    r.append(i)
-                rs = tuple(r)
-                print(rs)
-                rs = cur.fetchall()
-                return render_template('results.html', result=rs)
-
-            elif season == '5':
-                cur.execute('''SELECT * FROM season05 ''')
-                rs = list(rs)
-                r = []
-                for i in rs:
-                    i = list(i)
-                    i.insert(1,season)
-                    i = tuple(i)
-                    r.append(i)
-                rs = tuple(r)
-                print(rs)
-                rs = cur.fetchall()
-                return render_template('results.html', result=rs)
-
-            elif season == '6':
-                cur.execute('''SELECT * FROM season06''')
-                rs = list(rs)
-                r = []
-                for i in rs:
-                    i = list(i)
-                    i.insert(1,season)
-                    i = tuple(i)
-                    r.append(i)
-                rs = tuple(r)
-                print(rs)
-                rs = cur.fetchall()
-                return render_template('results.html', result=rs,**data)
-        elif deal:
-            if deal == 'Yes':
-                cur.execute('''SELECT * FROM dealproject''')
-                rs = cur.fetchall()
-        else:
-            rs = "Error! No input selected"
-            print(rs)
-            abort(404)
 
     else:
        abort(404)
